6eb30533f3fcbbd94498bf6860aef8a439e7a34d
Gustavo Sampaio <gbritosampaio@gmail.com>
2019-08-23 19:16:35 -0300

    Bump version
    Signed-off-by: Gustavo Sampaio <gbritosampaio@gmail.com>

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-wget https://github.com/runletapp/runlet/releases/download/v1.0.0-alpha.2/runlet-daemon_1.0.0-alpha.2_amd64.deb -O /tmp/runlet.deb
+wget https://github.com/runletapp/runlet/releases/download/1.0.0-alpha.2/runlet-daemon_1.0.0-alpha.2_amd64.deb -O /tmp/runlet.deb
 
 ls -lah /tmp
 
