eeba3a6713c19514755131d6f18c81b421a9291a
Vandre Leal <vandre.leal.candido@gmail.com>
2020-07-11 14:00:15 -0300

    (version) bump to 1.0.0

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-wget https://github.com/runletapp/runlet/releases/download/1.0.0-beta.5/runlet-daemon_1.0.0-beta.5_amd64.deb -O /tmp/runlet.deb
+wget https://github.com/runletapp/runlet/releases/download/1.0.0/runlet-daemon_1.0.0_amd64.deb -O /tmp/runlet.deb
 
 ls -lah /tmp
 
