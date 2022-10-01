adf7439cce8fd281c2a9f9a28680d72cd0c4aa99
Vandre Leal <vandre.leal.candido@gmail.com>
2022-10-01 10:17:52 -0300

    (version) bump to 1.0.8

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-wget https://github.com/runletapp/runlet/releases/download/1.0.7/runlet-daemon_1.0.7_amd64.deb -O /tmp/runlet.deb
+wget https://github.com/runletapp/runlet/releases/download/1.0.8/runlet-daemon_1.0.8_amd64.deb -O /tmp/runlet.deb
 
 ls -lah /tmp
 
